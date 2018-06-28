1bd8ded1fcc9d3fd33970638694da060a2106584
tavares1 <lucastavaresvasconcelos@gmail.com>
2018-06-28 09:35:15 -0300

    Preparando telas e outlets

--- Conversores/ViewController.swift
+++ Conversores/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tfValue: UITextField!
+    @IBOutlet weak var btnUnit1: UIButton!
+    @IBOutlet weak var btnUnit2: UIButton!
+    @IBOutlet weak var lbResult: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    @IBAction func showNext(_ sender: Any) {
+    
+    }
+    
+    @IBAction func convert(_ sender: Any) {
+    
+    }
 }
 
